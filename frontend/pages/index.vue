447c893064c20078abda5e06c73654128fa6f17a
peet5228 <ppeet5228@gmail.com>
2026-01-12 11:06:36 +0700

    ;;;

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <v-container>
-        <v-row justify="center"></v-row>
+    <v-container fluid class="fill-height bg-gray-4">
+        <v-row justify="center" align="center">llllll</v-row>
     </v-container>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({
+  layout: false
+})
 </script>
 
 <style scoped>
